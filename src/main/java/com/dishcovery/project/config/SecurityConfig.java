81d44e9c5d45917a80c63fe74214562d03df6681
bidrihi <bidrihi@gmail.com>
2025-01-23 15:04:28 +0900

    CustomAuthenticationFailureHandler 위치 수정

--- src/main/java/com/dishcovery/project/config/SecurityConfig.java
+++ src/main/java/com/dishcovery/project/config/SecurityConfig.java
@@ -1,7 +1,7 @@
 package com.dishcovery.project.config;
 
 import com.dishcovery.project.service.CustomUserDetailsService;
-import com.dishcovery.project.util.CustomAuthenticationFailureHandler;
+import com.dishcovery.project.service.CustomAuthenticationFailureHandler;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;

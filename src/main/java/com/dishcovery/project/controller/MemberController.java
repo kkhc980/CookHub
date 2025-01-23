c0dcd3c6f78ff31f7a46bf0d9ef882889cfa0bc4
bidrihi <bidrihi@gmail.com>
2025-01-23 12:41:04 +0900

    비밀번호 찾기

--- src/main/java/com/dishcovery/project/controller/MemberController.java
+++ src/main/java/com/dishcovery/project/controller/MemberController.java
@@ -14,12 +14,12 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpSession;
+import java.security.SecureRandom;
+import java.util.Date;
+import java.util.HashMap;
 import java.util.Map;
 
 @Controller
@@ -85,6 +85,71 @@ public class MemberController {
         return "redirect:/auth/login";
     }
 
+    // 인증 번호 보내기
+    @GetMapping("/mailCheck")
+    @ResponseBody
+    public String mailCheck(String email) {
+        log.info("mailCheck");
+        String authKey = mss.getKey(6);
+        Map<String, String> map = new HashMap<>();
+        map.put("email", email);
+        map.put("authKey", authKey);
+        int result = memberService.updateAuthKey(map);
+        log.info(result + " row update");
+        mss.sendVerificationCode(email, authKey);
+        log.info("mail send");
+        return authKey;
+    }
+
+    // 임시 비밀번호 발송
+    @PostMapping("/sendTempPassword")
+    @ResponseBody
+    public String sendTempPassword(@RequestParam("email") String email) {
+        try {
+            // 임시 비밀번호 생성
+            String tempPw = getRandomPassword(8);
+            Map<String, String> map = new HashMap<>();
+            map.put("email", email);
+            map.put("tempPw", passwordEncoder.encode(tempPw));
+            int result = memberService.updateTempPw(map);
+
+            if (result > 0) {
+                mss.sendTemporaryPassword(email, tempPw);
+                log.info("임시 비밀번호 발송 완료");
+                return "success";
+            } else {
+                log.info("임시 비밀번호 발송 실패");
+                return "fail";
+            }
+        } catch (Exception e) {
+            log.error(e.getMessage());
+            return "fail";
+        }
+    }
+
+    private String getRandomPassword(int size) {
+        char[] charSet = new char[]{'0', '1', '2', '3', '4', '5', '6', '7', '8', '9',
+                'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N',
+                'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'a', 'b',
+                'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p',
+                'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', '!', '@', '#', '$',
+                '%', '^', '&'
+        };
+        StringBuffer sb = new StringBuffer();
+        SecureRandom sr = new SecureRandom();
+        sr.setSeed(new Date().getTime());
+
+        int idx = 0;
+        int len = charSet.length;
+        for (int i = 0; i < size; i++) {
+            idx = sr.nextInt(len);
+
+            sb.append(charSet[idx]);
+        }
+
+        return sb.toString();
+    }
+
     @GetMapping("/detail")
     public String moveMemberDetailPage(Model model, @AuthenticationPrincipal UserDetails userDetails) {
         // @AuthenticationPrincipal : 인증된 사용자의 Principal을 주입
@@ -134,6 +199,14 @@ public class MemberController {
     public String moveFindPassword(Model model) {
         log.info("moveFindPassword");
         model.addAttribute("pageContent", "member/findpw.jsp");
+
+        return "layout";
+    }
+
+    @GetMapping("/updatepw")
+    public String moveUpdatePassword(Model model) {
+        log.info("moveUpdatePassword");
+        model.addAttribute("pageContent", "member/updatepw.jsp");
 
         return "layout";
     }
--- src/main/java/com/dishcovery/project/service/MailSendService.java
+++ src/main/java/com/dishcovery/project/service/MailSendService.java
@@ -63,4 +63,43 @@ public class MailSendService {
         }
         // return authKey;
     }
+
+    //인증코드 메일 보내기
+    public void sendVerificationCode(String email, String verificationCode) {
+        MimeMessage mail = mailSender.createMimeMessage();
+        try {
+            MimeMessageHelper helper = new MimeMessageHelper(mail, true, "UTF-8");
+            String mailContent = "<html><head><meta charset='UTF-8'></head><body>" +
+                    "<h1>[인증번호]</h1><br><p>인증번호는 다음과 같습니다. <br>" + verificationCode + "</p>"
+                    + "<br>해당 인증번호를 인증번호 확인란에 기입하여 주세요."
+                    + "</body></html>";
+
+            helper.setSubject("비밀번호 찾기 인증번호");
+            helper.setText(mailContent, true); // 두 번째 인자를 true 로 설정하면 HTML 메일로 처리
+            helper.setTo(email);
+            mailSender.send(mail);
+        } catch (MessagingException e) {
+            e.printStackTrace();
+        }
+    }
+
+    //임시비밀번호 보내기
+    @Async
+    public void sendTemporaryPassword(String email, String tempPw){
+        //인증메일 보내기
+        MimeMessage mail = mailSender.createMimeMessage();
+        try {
+            MimeMessageHelper helper = new MimeMessageHelper(mail, true, "UTF-8");
+            String mailContent = "<html><head><meta charset='UTF-8'></head><body>" +
+                    "<h1>임시 비밀번호</h1><br><p>아래 임시 비밀번호로 로그인 하신 후 비밀번호를 변경하시길 바랍니다.</p><br>" +
+                    tempPw + "</body></html>";
+
+            helper.setSubject("임시 비밀번호");
+            helper.setText(mailContent, true); // 두 번째 인자를 true 로 설정하면 HTML 메일로 처리
+            helper.setTo(email);
+            mailSender.send(mail);
+        } catch (MessagingException e) {
+            e.printStackTrace();
+        }
+    }
 }
